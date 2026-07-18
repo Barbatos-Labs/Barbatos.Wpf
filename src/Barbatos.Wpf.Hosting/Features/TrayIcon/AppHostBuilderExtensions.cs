1355a2ab79e907b1a7cb00ed816e8778b6d91742
StHung <phamthehung2212000@gmail.com>
2026-07-18 22:19:40 +0700

    rename solution

--- src/Barbatos.Wpf.Hosting/Features/TrayIcon/AppHostBuilderExtensions.cs
+++ src/Barbatos.Wpf.Hosting/Features/TrayIcon/AppHostBuilderExtensions.cs
@@ -1,6 +1,6 @@
 // This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
-// Copyright (C) Barbatos Labs | Pham The Hung and Barbatos.Wpf.Core Contributors.
+// Copyright (C) Barbatos Labs | Pham The Hung and Barbatos.Wpf Contributors.
 // All Rights Reserved.
 
 using Barbatos.Wpf.Tray;
